6d78df01e2481b2ec13d252316f95b10ff50b8a4
Edmund Wippler <ed.wippler@gmail.com>
2017-03-11 17:17:36 -0600

    table displaying test data using angular binding

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,4 +6,12 @@ app.controller('EmployeeListController', function () {
   var self = this;
 
   self.message = 'Digital wizardry';
+  self.testList = [
+    {first_name:'Dave', last_name: 'Schwimmer', eidn: '100A', job_title: 'Scuba Instructor', annual_salary: 48000},
+    {first_name:'Ginger', last_name: 'Vitus', eidn: '325C', job_title: 'Dentist', annual_salary: 96000},
+    {first_name:'Marty', last_name: 'McFly', eidn: '1964', job_title: 'Time Travel Intern', annual_salary: 12},
+    {first_name:'Doc', last_name: 'Brown', eidn: 'DL88', job_title: 'Applied Science Specialist', annual_salary: 88888}
+  ];
+
+
 });
